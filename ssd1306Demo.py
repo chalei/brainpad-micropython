a0a41ccd9241ebacbc0e759f292259f12200601b
Hendra Kusumah <h.kusumah87@gmail.com>
2017-09-04 00:28:53 +0700

    Update ssd1306Demo.py

--- ssd1306Demo.py
+++ ssd1306Demo.py
@@ -1,1 +1,11 @@
-#hardware platform: pyboard V1.1import pybimport ssd1306i2c=pyb.I2C(1,pyb.I2C.MASTER,baudrate=100000)lcd=ssd1306.SSD1306_I2C(128,64,i2c)lcd.text("The BrainPad",0,0)lcd.text("From ",0,16)lcd.text("GHI Electronics",0,32)lcd.show()
+#hardware platform: Brainpad
+#Testing the onboard Oled
+import pyb
+import ssd1306
+
+i2c=pyb.I2C(1,pyb.I2C.MASTER,baudrate=100000)
+lcd=ssd1306.SSD1306_I2C(128,64,i2c)
+lcd.text("The BrainPad",0,0)
+lcd.text("From ",0,16)
+lcd.text("GHI Electronics",0,32)
+lcd.show()
