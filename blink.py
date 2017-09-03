0f55eeebf237f835b9b6a20424c9209cf84b9403
Hendra Kusumah <h.kusumah87@gmail.com>
2017-09-04 00:26:15 +0700

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,1 +1,12 @@
-from pyb import Pinimport timeled = Pin('C8',Pin.OUT)while True:  led.value(1)  time.sleep(0.5)  led.value(0)  time.sleep(0.5)
+#blink Onboard LED
+
+from pyb import Pin
+import time
+
+led = Pin('C8',Pin.OUT)
+
+while True:
+  led.value(1)
+  time.sleep(0.5)
+  led.value(0)
+  time.sleep(0.5)
