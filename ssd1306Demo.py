c177ca7c64567f4d333185c49fc46fa3c819e246
Hendra Kusumah <h.kusumah87@gmail.com>
2017-09-04 00:17:37 +0700

    Add files via upload

---
+++ ssd1306Demo.py
@@ -0,0 +1,1 @@
+#hardware platform: pyboard V1.1import pybimport ssd1306i2c=pyb.I2C(1,pyb.I2C.MASTER,baudrate=100000)lcd=ssd1306.SSD1306_I2C(128,64,i2c)lcd.text("The BrainPad",0,0)lcd.text("From ",0,16)lcd.text("GHI Electronics",0,32)lcd.show()
